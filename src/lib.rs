8528f5a9a7c922c9630cd0467ee0a07ec54da7bf
Pete LeVasseur <plevasseur@gmail.com>
2025-04-10 10:17:21 +0900

    Add receive

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,22 @@
-use up_core_api_wit::*;
+use up_core_api_wit::up_core_api::{Umessage, Uuri};
+use up_core_api_wit::up_l1::UTransport;
 
+struct UTransportMock;
 
+impl UTransport for UTransportMock {
+    fn register_listener(_: Uuri, _: Uuri) -> wit_bindgen_rt::async_support::FutureReader<String> {
+        todo!()
+    }
+    fn unregister_listener(
+        _: Uuri,
+        _: Uuri,
+    ) -> wit_bindgen_rt::async_support::FutureReader<String> {
+        todo!()
+    }
+    fn send(_: Umessage) -> wit_bindgen_rt::async_support::FutureReader<String> {
+        todo!()
+    }
+    fn receive(_: Uuri, _: Uuri) -> FutureReader<Umessage> {
+        todo!()
+    }
+}
