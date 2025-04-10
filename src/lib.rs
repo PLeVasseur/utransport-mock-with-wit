749725a26b6ab0034e6998620a0a21a694d0e59a
Pete LeVasseur <plevasseur@gmail.com>
2025-04-10 10:34:21 +0900

    Add UStatus and UCode

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,21 @@
-use up_core_api_wit::up_core_api::{Umessage, Uuri};
+use up_core_api_wit::up_core_api::{Umessage, Uuri, Ustatus};
 use up_core_api_wit::up_l1::UTransport;
+
+use wit_bindgen_rt::async_support::FutureReader;
 
 struct UTransportMock;
 
 impl UTransport for UTransportMock {
-    fn register_listener(_: Uuri, _: Uuri) -> wit_bindgen_rt::async_support::FutureReader<String> {
+    fn register_listener(_: Uuri, _: Uuri) -> FutureReader<Ustatus> {
         todo!()
     }
     fn unregister_listener(
         _: Uuri,
         _: Uuri,
-    ) -> wit_bindgen_rt::async_support::FutureReader<String> {
+    ) -> FutureReader<Ustatus> {
         todo!()
     }
-    fn send(_: Umessage) -> wit_bindgen_rt::async_support::FutureReader<String> {
+    fn send(_: Umessage) -> FutureReader<Ustatus> {
         todo!()
     }
     fn receive(_: Uuri, _: Uuri) -> FutureReader<Umessage> {
